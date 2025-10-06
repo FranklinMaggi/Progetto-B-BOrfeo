48171293dd9f4a50fed62e5bdf7f1232c6a63850
Francesco Maggi <franklin19@outlook.it>
2025-10-06 02:47:43 +0200

    change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://franklinmaggi.github.io/Progetto-B-BOrfeo/',
+  base: '/Progetto-B-BOrfeo/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
